d4bf89b6daad9e40aff2c2576a4d35d540a117e9
SacshamAgrawal <sacshamagrawal28@gmail.com>
2020-03-30 15:31:12 +0530

    new commit

--- scrum/settings.py
+++ scrum/settings.py
@@ -76,7 +76,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'scrum',
         'USER': 'codex28',
-        'PASSWORD': 'abcd',
+        'PASSWORD': 'askingdemon',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
